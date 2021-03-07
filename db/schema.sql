60a2f35458464dbaf0485d32666523797bf5a026
devontehillman <hillmand@mail.gvsu.edu>
2021-03-07 10:29:50 -0500

    Update schema.sql
    Initial

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXIST burgers_db
+
+USE bugers_db 
+
+CREATE TABLE burgers (
+id INT auto_increment NOT NULL,
+burger_name VARCHAR(25),
+devoured BOOLEAN,
+primary id (id)
+)
+
+INSERT INTO burgers (burger_name, devoured)
+VAlUE()
