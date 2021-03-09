8982db321d473024b168f16dec1d7581ce033de6
devontehillman <hillmand@mail.gvsu.edu>
2021-03-09 17:08:24 -0500

    Update seed.sql
    Added seeds

---
+++ db/seed.sql
@@ -0,0 +1,9 @@
+USE burgers_db;
+
+
+INSERT INTO burgers (burger_name, devoured)
+VAlUES('Craby Patty', false);
+INSERT INTO burgers (burger_name, devoured)
+VAlUES('Chick Fillet', true );
+INSERT INTO burgers (burger_name, devoured)
+VAlUES('Fary odd Burger', false);
