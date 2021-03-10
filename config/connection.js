1475d8f5d538439978cda4c0a9f4cf0c846db69d
Devonte Hillman <hillmand@mail.gvsu.edu>
2021-03-10 16:50:21 -0500

    returned to original and added config vat

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,18 @@ require('dotenv').config();
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  
+  host: "localhost",
   port: 3306,
   user: "root",
   password: process.env.DB_PASSWORD,
   database: "burgers_db",
 });
 
-connection.connect(() => {
-  
+connection.connect((err) => {
+  if (err) {
+    console.error(`error connecting: ${err.stack}`);
+    return;
+  }
   console.log(`connected as id ${connection.threadId}`);
 });
 
