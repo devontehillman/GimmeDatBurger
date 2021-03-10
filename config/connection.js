e2f33417fe13ce218743e58ea0afdcf7a4f0f167
Devonte Hillman <hillmand@mail.gvsu.edu>
2021-03-10 16:30:31 -0500

    removed local host

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  host: "localhost",
+  
   port: 3306,
   user: "root",
   password: process.env.DB_PASSWORD,
